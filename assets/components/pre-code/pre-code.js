a83fe9dceb5401a6f800baaddd1b7050b498dae0
Austin Stewart <hello@austinthedeveloper.com>
2016-05-07 20:19:06 -0500

    Created a pre-code component so it can be seen when an admin is logged in.

---
+++ assets/components/pre-code/pre-code.js
@@ -0,0 +1,14 @@
+angular.module('wpAngularTheme')
+.component('preCode', {
+	bindings: {
+		data: '='
+	},
+	templateUrl: components + 'pre-code/pre-code.html',
+	controller: [function() {
+		var vm = this;
+
+		vm.isAdmin = ajaxInfo.is_admin;
+
+		console.log("ADMIN: ", vm.isAdmin);
+	}]
+})
