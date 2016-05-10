771422a5d8bc5b944907823357930dc71e17b12c
Austin Stewart <hello@austinthedeveloper.com>
2016-05-09 22:35:49 -0500

    Added a "Dummy Text" component

--- assets/components/single/single.js
+++ assets/components/single/single.js
@@ -1,11 +1,14 @@
 angular.module('wpAngularTheme')
 .component('single', {
 	templateUrl: components + 'single/single.html',
-	controller: ['$stateParams','PostsBySlug','Comments',function($stateParams,PostsBySlug,Comments){
+	controller: ['$stateParams','PostsBySlug','Comments', 'Processing', function($stateParams,PostsBySlug,Comments, Processing){
 		var vm = this;
+
+		Processing.on();
 
 		PostsBySlug.get($stateParams,function(res){
 			vm.post = res.post;
+			Processing.off();
 		});
 		
 	}]
