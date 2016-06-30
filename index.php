77fe1751019326ed7a6621b57dd02971c8bdf159
Austin Stewart <hello@austinthedeveloper.com>
2016-06-30 11:25:57 -1000

    Removed layout-padding from ui-view and added autoscroll on state change.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 	<processing></processing>
 
-	<div class="main-content" ui-view layout-padding>
+	<div class="main-content" ui-view autoscroll="true">
 
 	</div>
 <?php get_footer(); ?>
